5e05aed3d659532c5b24124d3bd18d16a1038d9b
Mitchell Henke <mitchh23@gmail.com>
2014-04-14 19:54:35 -0500

    Have users load pg_trgm extension themselves

--- lib/sequel/extensions/pg_trgm.rb
+++ lib/sequel/extensions/pg_trgm.rb
@@ -1,16 +1,6 @@
 module Sequel
   module Postgres
     module PgTrgm
-      def add_extension(name)
-        quoted_name = quote_identifier(name) if name
-        self.run "CREATE EXTENSION IF NOT EXISTS #{quoted_name};"
-      end
-
-      def drop_extension(name)
-        quoted_name = quote_identifier(name) if name
-        self.run "DROP EXTENSION IF EXISTS #{quoted_name};"
-      end
-
       def add_pg_trgm(table, column, op={})
         self.run "CREATE INDEX #{table}_#{column}_trgm_index ON #{table} USING gist (#{column} gist_trgm_ops);"
       end
